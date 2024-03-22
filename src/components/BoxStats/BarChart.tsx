b451c20332889e1e1058df22c22418968b2ec280
louis-stintzy <louis.stintzy@gmail.com>
2024-03-22 12:28:31 +0100

    Add bar chart for instant statistics

---
+++ src/components/BoxStats/BarChart.tsx
@@ -0,0 +1,105 @@
+import { Bar } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface CardsByCompartment {
+  cardsByCompartment: {
+    compartment1: number;
+    compartment2: number;
+    compartment3: number;
+    compartment4: number;
+    compartment5: number;
+    compartment6: number;
+    compartment7: number;
+    compartment8: number;
+  };
+}
+
+function BarChart({ cardsByCompartment }: CardsByCompartment) {
+  const data = {
+    labels: [
+      'Compartment 1',
+      'Compartment 2',
+      'Compartment 3',
+      'Compartment 4',
+      'Compartment 5',
+      'Compartment 6',
+      'Compartment 7',
+      'Compartment 8',
+    ],
+    datasets: [
+      {
+        label: 'Number of cards',
+        data: [
+          cardsByCompartment.compartment1,
+          cardsByCompartment.compartment2,
+          cardsByCompartment.compartment3,
+          cardsByCompartment.compartment4,
+          cardsByCompartment.compartment5,
+          cardsByCompartment.compartment6,
+          cardsByCompartment.compartment7,
+          cardsByCompartment.compartment8,
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    maintainAspectRatio: false, // Permet une hauteur fixe tout en étant réactif en largeur -> responsive
+    indexAxis: 'y' as const,
+    scales: {
+      y: {
+        beginAtZero: true,
+        // ticks: {
+        //   autoSkip: false,
+        // },
+      },
+    },
+  };
+  return <Bar data={data} options={options} />;
+}
+
+export default BarChart;
--- src/components/BoxStats/BoxInstantStats.tsx
+++ src/components/BoxStats/BoxInstantStats.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { StatsData } from '../../@types/stats';
+import BarChart from './BarChart';
 
 interface BoxStats {
   boxStats: StatsData;
@@ -8,33 +9,38 @@ interface BoxStats {
 function BoxInstantStats({ boxStats }: BoxStats) {
   return (
     <Box>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Total cards: {boxStats.totalCards}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 1 : {boxStats.cardsByCompartment.compartment1}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 2 : {boxStats.cardsByCompartment.compartment2}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 3 : {boxStats.cardsByCompartment.compartment3}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 4 : {boxStats.cardsByCompartment.compartment4}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 5 : {boxStats.cardsByCompartment.compartment5}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 6 : {boxStats.cardsByCompartment.compartment6}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 7 : {boxStats.cardsByCompartment.compartment7}
-      </Typography>
-      <Typography variant="h6" component="h2" gutterBottom>
-        Compartment 8 : {boxStats.cardsByCompartment.compartment8}
-      </Typography>
+      <Box sx={{ height: '500px', width: '100%' }}>
+        <BarChart cardsByCompartment={boxStats.cardsByCompartment} />
+      </Box>
+      <Box>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Total cards: {boxStats.totalCards}
+        </Typography>
+        {/* <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 1 : {boxStats.cardsByCompartment.compartment1}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 2 : {boxStats.cardsByCompartment.compartment2}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 3 : {boxStats.cardsByCompartment.compartment3}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 4 : {boxStats.cardsByCompartment.compartment4}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 5 : {boxStats.cardsByCompartment.compartment5}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 6 : {boxStats.cardsByCompartment.compartment6}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 7 : {boxStats.cardsByCompartment.compartment7}
+        </Typography>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Compartment 8 : {boxStats.cardsByCompartment.compartment8}
+        </Typography> */}
+      </Box>
     </Box>
   );
 }
