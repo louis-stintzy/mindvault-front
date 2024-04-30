16059db32f4c21141f23dc9138e4083095864d3c
louis-stintzy <louis.stintzy@gmail.com>
2024-04-30 07:03:30 +0200

    Read the questions and answers in the selected voices

--- src/components/Play/CardTwoSides.tsx
+++ src/components/Play/CardTwoSides.tsx
@@ -1,5 +1,5 @@
 import { Box, Container, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import BottomNavigationMUI from '../BottomNavigationMUI/BottomNavigationMUI';
 import { CardData } from '../../@types/card';
 import { useAppDispatch, useAppSelector } from '../../hook/redux';
@@ -19,6 +19,12 @@ function CardTwoSides({ card, goToNextCard }: QuestionProps) {
   const [initialCompartment, setInitialCompartment] = useState(
     card.compartment
   );
+  const [voicesInitialized, setVoicesInitialized] = useState(false);
+  // la vérification de la disponibilité de la voix s'effectue directement dans speakText.
+  // On ne vérifie pas si la voix est disponible avant dans le useState, ce n'est pas necessaire car si pas dispo on utilisera la voix par défaux
+  // const [availableVoicesName, setAvailableVoicesName] = useState<string[]>(
+  //   speechSynthesis.getVoices().map((voice) => voice.name)
+  // );
   const [userAnswer, setUserAnswer] = useState('');
   const [isCorrect, setIsCorrect] = useState(false);
   const { autoRead } = useAppSelector((state) => state.cardMultiple);
@@ -31,12 +37,27 @@ function CardTwoSides({ card, goToNextCard }: QuestionProps) {
       setAnswerLanguage(value);
     };
 
-  const speakText = (text: string, lang: string) => {
-    // todo : voix par défaut si la voix choisie n'est pas disponible
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = lang;
-    speechSynthesis.speak(utterance);
-  };
+  const speakText = useCallback(
+    (text: string, lang: string, voiceName?: string) => {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = lang;
+
+      if (voiceName) {
+        const voiceToUSe = speechSynthesis
+          .getVoices()
+          .find((voice) => voice.name === voiceName);
+        if (!voiceToUSe) {
+          console.error(
+            'Voice not found, ce n est pas grave, on prend la voix par défaut'
+          );
+        } else {
+          utterance.voice = voiceToUSe;
+        }
+      }
+      speechSynthesis.speak(utterance);
+    },
+    []
+  );
 
   const verifyAnswer = (answerToCheck: string, correctAnswer: string) => {
     return (
@@ -94,21 +115,49 @@ function CardTwoSides({ card, goToNextCard }: QuestionProps) {
     handleNextButton();
   };
 
+  // ----------- USE EFFECTS -------------
+  // -------- Load card attributes -------
   useEffect(() => {
     setInitialCompartment(card.compartment);
     setAnswerLanguage(card.answerLanguage);
-    if (autoRead.question) {
-      speakText(card.question, card.questionLanguage);
-    }
-  }, [card, autoRead.question]);
-
+  }, [card]);
+  // ------- Load voices available -------
   useEffect(() => {
-    if (isFlipped) {
-      if (autoRead.answer) {
+    // si le chargement s'effectue en plusieurs fois et que le texte se répète inclure un setTimeout en plus pour que setVoicesInitialized ne passe à true qu'au bout d'un certain temps
+    const handleVoicesChanged = () => {
+      if (!voicesInitialized) {
+        setVoicesInitialized(true);
+      }
+      // setAvailableVoicesName(
+      //   speechSynthesis.getVoices().map((voice) => voice.name)
+      // );
+    };
+    handleVoicesChanged();
+    speechSynthesis.onvoiceschanged = handleVoicesChanged;
+    return () => {
+      speechSynthesis.onvoiceschanged = null;
+    };
+  }, [voicesInitialized]);
+  // -------------- Autoread Question--------------
+  useEffect(() => {
+    if (voicesInitialized && autoRead.question && !isFlipped) {
+      if (card.questionVoice) {
+        speakText(card.question, card.questionLanguage, card.questionVoice);
+      } else {
+        speakText(card.question, card.questionLanguage);
+      }
+    }
+  }, [card, autoRead.question, isFlipped, voicesInitialized, speakText]);
+  // -------------- Autoread Answer--------------
+  useEffect(() => {
+    if (voicesInitialized && autoRead.answer && isFlipped) {
+      if (card.answerVoice) {
+        speakText(card.answer, card.answerLanguage, card.answerVoice);
+      } else {
         speakText(card.answer, card.answerLanguage);
       }
     }
-  }, [isFlipped, card, autoRead.answer]);
+  }, [card, autoRead.answer, isFlipped, voicesInitialized, speakText]);
 
   return (
     <Container component="main" maxWidth="xs" className="question-container">
